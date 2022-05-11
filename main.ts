46638c10bf83aeb6492611599c9594d61287f9bb
Tsukitai <103410207+Tsukitai@users.noreply.github.com>
2022-05-11 09:10:05 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+input.onSound(DetectedSound.Loud, function () {
+    input.setSoundThreshold(SoundThreshold.Loud, 100)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onSound(DetectedSound.Quiet, function () {
+    input.setSoundThreshold(SoundThreshold.Quiet, 99)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
 })
